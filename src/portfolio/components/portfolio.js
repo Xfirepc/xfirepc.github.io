7828e99657fbb609db334508d0e7b0d567d1e0c3
Xfirepc <vinygfx@gmail.com>
2018-03-28 18:42:43 -0500

    add events for clicks

---
+++ src/portfolio/components/portfolio.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function PortfolioContent (props) {
+
+  if(props.item != ''){
+    return props.item.map((data)=> {
+
+      console.log(data)
+      return (<div key={data.id}>
+        <div className="isotope-item photography wow fadeInUp">
+        	<div className="portfolio-item-wrap">
+									<div className="portfolio-item">
+										<a ref={props.setRef} className="item-link inline-popup-trigger" onClick={props.handleClick} href={`#portfolio-${data.id}`}>
+											<span className="cover bg-transparent-9-main"></span>
+											<img className="item-img" src="assets/img/portfolio/masonry/portfolio-img-4.jpg" alt="image"/>
+											<div className="item-info text-white">
+												<h3 className="item-info-title">{data.title}</h3>
+												<p className="item-info-text">{data.catedory}</p>
+											</div>
+										</a>
+									</div>
+
+									<div id={`portfolio-${data.id}`} className="inline-popup mfp-hide">
+										<div className="inline-popup-inner">
+
+											<div className="inline-popup-image bg-image" style={{backgroundImage: 'url(assets/img/portfolio/big/portfolio-img-4.jpg)', backgroundPosition: '50% 50%'}}></div>
+
+											<div className="row margin-top-40">
+												<div className="col-md-8 margin-bottom-20">
+													<h2 className="no-margin-top">{data.name}</h2>
+													<p>{data.description}</p>
+												</div>
+												<div className="col-md-4 margin-bottom-20 padding-left-40">
+													<ul className="list-unstyled">
+														<li>Client: <strong><a>{data.client}</a></strong></li>
+														<li>Date: <strong><a>{data.date}</a></strong></li>
+														<li>Category <strong><a>{data.category}</a></strong></li>
+														<li>Copyright ©2016, All Rights Reserved</li>
+													</ul>
+													<a href={data.link} target="_blank" className="btn btn-primary btn-rounded btn-lg margin-top-20">View Project</a>
+												</div>
+											</div>
+
+										</div>
+
+										<a className="inline-popup-close" href="#"><i className="fa fa-times"></i></a>
+
+									</div>
+								</div>
+              </div>
+      </div>)
+
+    })
+  }
+
+  return null
+}
+
+export default PortfolioContent
--- src/portfolio/container/port.js
+++ src/portfolio/container/port.js
@@ -7,29 +7,49 @@ class Portfolio extends Component{
   state = {
     portf: ''
   }
-
+  
   componentWillMount =  async() =>{
+
     this.setState({
       portf: await getUrlApi(this.props.url.portfolio)
     })
-    this.state.portf.map(( item ) => {
-      return console.log('Item ', item)
-    })
+    
   }
 
+  launchModal = (ev) =>{
+    
+    $('.inline-popup-trigger').magnificPopup({
+      type: 'inline',
+      modal: false,
+      alignTop: true,
+      fixedContentPos: true,
+      fixedBgPos: false,
+      overflowY: 'auto',
+      closeBtnInside: true,
+      preloader: false,
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'mfp-fade-zoom',
+      gallery: {
+          enabled: true, // enable or disable gallery (false/true)
+          arrowMarkup: '<button title="%title%" type="button" class="mfp-custom-arrow mfp-custom-arrow-%dir%"></button>', // markup of an arrow button
+          tPrev: 'Previous (Left arrow key)', // title for left button
+          tNext: 'Next (Right arrow key)' // title for right button
+      }
+    });
+    
+  
+
+  }
+  setRef = (element) => {
+    this.tigger = element
+    console.log(this.tigger)
+  }
+  
   render(){
     return(
       <div>
-      {
-        this.state.portf ?
-        <div>
-          {this.state.portf.map((item)=>{
-            return <PortfolioContent {...item}/>
-          })}
-        </div>
-        :
-        <div>No se ha cargado</div>
-      }
+        <PortfolioContent setRef={this.setRef} handleClick={this.launchModal} item = {this.state.portf}/>
       </div>
     )
   }
